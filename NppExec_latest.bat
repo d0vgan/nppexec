51af9acd6b886c906cce3d8662cb3e7e590e71f5
d0vgan <dvv81@ukr.net>
2018-06-06 00:01:36 +0300

    Now NppExec_dll*.zip includes "README.md" as "doc\NppExec\README.txt"

--- NppExec_latest.bat
+++ NppExec_latest.bat
@@ -1,5 +1,8 @@
 @echo off
 
+sfk copy README.md NppExec\doc\NppExec\README.txt -yes
+sfk copy NppExec\doc\NppExec\NppExec_Manual\NppExec_Manual.chm NppExec\doc\NppExec\ -yes
+
 set ARC_EXE=7z.exe
 for /f "tokens=1-3 delims=/.- " %%a in ('DATE /T') do set ARC_DATE=%%c%%b%%a
 set ARC_SRC_NAME=NppExec%ARC_DATE%_src
@@ -13,7 +16,7 @@ if not exist NppExec\Release\NppExec.dll goto no_dll_file_exit
 cd NppExec\Release
 %ARC_EXE% u -tzip ..\..\%ARC_DLL_NAME%.zip NppExec.dll NppExec\*.h -mx5
 cd ..
-%ARC_EXE% u -tzip ..\%ARC_DLL_NAME%.zip doc\NppExec\NppExec.txt doc\NppExec\NppExec_TechInfo.txt doc\NppExec\NppExec*.chm doc\NppExec\*.html doc\NppExec\*.css doc\NppExec\*.png -mx5
+%ARC_EXE% u -tzip ..\%ARC_DLL_NAME%.zip doc\NppExec\README.txt doc\NppExec\NppExec.txt doc\NppExec\NppExec_TechInfo.txt doc\NppExec\NppExec*.chm doc\NppExec\*.html doc\NppExec\*.css doc\NppExec\*.png -mx5
 cd ..
 %ARC_EXE% t %ARC_DLL_NAME%.zip
 
@@ -23,7 +26,7 @@ if not exist NppExec\x64\Release\NppExec.dll goto no_dll_file_x64_exit
 cd NppExec\x64\Release
 %ARC_EXE% u -tzip ..\..\..\%ARC_DLL_NAME_64%.zip NppExec.dll -mx5
 cd ..\..
-%ARC_EXE% u -tzip ..\%ARC_DLL_NAME_64%.zip doc\NppExec\NppExec.txt doc\NppExec\NppExec_TechInfo.txt doc\NppExec\NppExec*.chm doc\NppExec\*.html doc\NppExec\*.css doc\NppExec\*.png -mx5
+%ARC_EXE% u -tzip ..\%ARC_DLL_NAME_64%.zip doc\NppExec\README.txt doc\NppExec\NppExec.txt doc\NppExec\NppExec_TechInfo.txt doc\NppExec\NppExec*.chm doc\NppExec\*.html doc\NppExec\*.css doc\NppExec\*.png -mx5
 cd Release
 %ARC_EXE% u -tzip ..\..\%ARC_DLL_NAME_64%.zip NppExec\*.h -mx5
 cd ..\..
