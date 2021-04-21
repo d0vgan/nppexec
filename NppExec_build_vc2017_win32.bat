59f50365a1939011f8f7351acf4cc51e22363583
d0vgan <dvv81@ukr.net>
2021-04-21 19:22:27 +0300

    Updating NppExec_build_vc2017_*.bat

--- NppExec_build_vc2017_win32.bat
+++ NppExec_build_vc2017_win32.bat
@@ -1,4 +1,28 @@
-call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+@echo off
+
+set VC_ROOT=%ProgramFiles(x86)%\Microsoft Visual Studio\2017
+
+if exist "%VC_ROOT%\Professional\VC\Auxiliary\Build\vcvarsall.bat" goto UseVcProfessional
+if exist "%VC_ROOT%\Community\VC\Auxiliary\Build\vcvarsall.bat" goto UseVcCommunity
+
+goto ErrorNoVcVarsAll
+
+:UseVcProfessional
+call "%VC_ROOT%\Professional\VC\Auxiliary\Build\vcvarsall.bat" x86
+goto Building
+
+:UseVcCommunity
+call "%VC_ROOT%\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+goto Building
+
+:Building
 cd .\NppExec
 msbuild NppExec_VC2017.sln /t:Clean /p:Configuration=Release;Platform=Win32
 msbuild NppExec_VC2017.sln /t:Build /p:Configuration=Release;Platform=Win32
+goto End
+
+:ErrorNoVcVarsAll
+echo ERROR: Could not find "vcvarsall.bat"
+goto End
+
+:End
