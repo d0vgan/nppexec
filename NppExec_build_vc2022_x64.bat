13b398a63f5e561b68320e5d6cfa6e11829c62c3
d0vgan <dvv81@ukr.net>
2023-05-17 23:48:46 +0300

    adding NppExec_build_vc2022_*.bat

---
+++ NppExec_build_vc2022_x64.bat
@@ -0,0 +1,28 @@
+@echo off
+
+set VC_ROOT=%ProgramFiles%\Microsoft Visual Studio\2022
+
+if exist "%VC_ROOT%\Professional\VC\Auxiliary\Build\vcvarsall.bat" goto UseVcProfessional
+if exist "%VC_ROOT%\Community\VC\Auxiliary\Build\vcvarsall.bat" goto UseVcCommunity
+
+goto ErrorNoVcVarsAll
+
+:UseVcProfessional
+call "%VC_ROOT%\Professional\VC\Auxiliary\Build\vcvarsall.bat" x86
+goto Building
+
+:UseVcCommunity
+call "%VC_ROOT%\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+goto Building
+
+:Building
+cd .\NppExec
+msbuild NppExec_VC2022.sln /t:Clean /p:Configuration=Release;Platform=x64 /m:2
+msbuild NppExec_VC2022.sln /t:Build /p:Configuration=Release;Platform=x64 /m:2
+goto End
+
+:ErrorNoVcVarsAll
+echo ERROR: Could not find "vcvarsall.bat"
+goto End
+
+:End
