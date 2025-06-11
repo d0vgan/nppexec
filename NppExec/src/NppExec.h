8f31eacc74a4917bb067bf640146be452757da1c
d0vgan <dvv81@ukr.net>
2025-06-11 15:28:33 +0300

    updating the docs

--- NppExec/src/NppExec.h
+++ NppExec/src/NppExec.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ------------------
  * Glory to Ukraine! Glory to the heroes!
  * changed: now $(#N) considers the current view (Primary or Secondary)
- * changed: nppGetOpenFileNames() has been replaced with nppGetOpenFileName()
+ * changed: new method nppGetOpenFileName() does not use NPPM_GETOPENFILENAMES
  * changed: further improvements in findFileNameIndexInNppOpenFileNames
  + NppExec Manual updated
  * npp_files updated to Notepad++ 8.8.2
