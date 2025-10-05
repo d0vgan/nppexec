5648344bf3d2224e93d767abb62a9c2108ae0535
d0vgan <dvv81@ukr.net>
2025-10-05 17:17:08 +0300

    updating the npp_files

--- NppExec/src/npp_files/menuCmdID.h
+++ NppExec/src/npp_files/menuCmdID.h
@@ -184,6 +184,8 @@
     #define    IDM_EDIT_MULTISELECTSSKIP                        (IDM_EDIT + 99)
     #define    IDM_EDIT_SORTLINES_LOCALE_ASCENDING              (IDM_EDIT + 100)
     #define    IDM_EDIT_SORTLINES_LOCALE_DESCENDING             (IDM_EDIT + 101)
+    #define    IDM_EDIT_SETREADONLYFORALLDOCS                   (IDM_EDIT + 102)
+    #define    IDM_EDIT_CLEARREADONLYFORALLDOCS                 (IDM_EDIT + 103)
 
     #define    IDM_EDIT_AUTOCOMPLETE                            (50000 + 0)
     #define    IDM_EDIT_AUTOCOMPLETE_CURRENTFILE                (50000 + 1)
@@ -594,10 +596,10 @@
 
 
 #define    IDM_SETTING    (IDM + 8000)
-//    #define    IDM_SETTING_TAB_SIZE                 (IDM_SETTING + 1)
-//    #define    IDM_SETTING_TAB_REPLACESPACE         (IDM_SETTING + 2)
-//    #define    IDM_SETTING_HISTORY_SIZE             (IDM_SETTING + 3)
-//    #define    IDM_SETTING_EDGE_SIZE                (IDM_SETTING + 4)
+//  #define    IDM_SETTING_TAB_SIZE                 (IDM_SETTING + 1)
+//  #define    IDM_SETTING_TAB_REPLACESPACE         (IDM_SETTING + 2)
+//  #define    IDM_SETTING_HISTORY_SIZE             (IDM_SETTING + 3)
+//  #define    IDM_SETTING_EDGE_SIZE                (IDM_SETTING + 4)
     #define    IDM_SETTING_IMPORTPLUGIN             (IDM_SETTING + 5)
     #define    IDM_SETTING_IMPORTSTYLETHEMES        (IDM_SETTING + 6)
     #define    IDM_SETTING_TRAYICON                 (IDM_SETTING + 8)
